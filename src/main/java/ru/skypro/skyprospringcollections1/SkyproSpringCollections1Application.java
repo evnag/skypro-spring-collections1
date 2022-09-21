1ee9f16c23c2cd740c197989ec917e9bd0105362
evnag <vskonev22@gmail.com>
2022-09-21 18:41:45 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/skyprospringcollections1/SkyproSpringCollections1Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.skyprospringcollections1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringCollections1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringCollections1Application.class, args);
+	}
+
+}
